745fbe54d289e795fcff14c340c4540d8be260ed
sarahmangiameli <sarahmangiameli@gmail.com>
2022-09-21 16:33:26 -0400

    Add bins file

--- scripts/figure3_preproc/generate_chrom_bins.sh
+++ scripts/figure3_preproc/generate_chrom_bins.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-#This file creates a bedfile containing genomic bins of a specified size
+#This file creates a bedfile containing genomic bins of a specified size. Note that the chrY, chrM
+#and any unplaced contigs are removed.
 
 #INPUTS:
-#	chrom_size: Two column Tab delimited file containing chromosome name and length
+#	chrom_size: Two column Tab delimited file containing chromosome name and length.
+#	The expected format is [genome ID].chrom.sizes
 #	bin_size: the desired output bin size in base pairs
 #	filter:
 
 #OUTPUTS:
 
 #EXAMPLE USAGE:
-#	generate_chrom_bins.sh reference/chrom_sizes/hg38.chrom.sizes 1000000 1
+#	generate_chrom_bins.sh reference/chrom_sizes/hg38.chrom.sizes reference/chrom_bins/hg38.1Mb_bins.filt.bed 1000000
 
 chrom_size=$1 #Chromosome size file
 bin_size=$2 #Bin size in base pairs
-filter=$3 #Logical - Keep only chrs 1-23
 
-#note that the hg38.chrom_filt.sizes was generated using filterGenomeLengthFile.sh
-if [ $filter -eq 1 ]
+#Generate the output file path
+genome_id=`basename $chrom_size | cut -d. -f1`
+output_name='reference/chrom_bins/'$genome_id'.'$bin_size'bp_bins.filt.bed'
+
+#If old file exists, remove it
+if [ -f "$output_name" ]
 then
-	genomeSizeFile='/broad/thechenlab/atac_pipeline_bc/genomes/chrom.sizes/hg38.chrom_filt.sizes'
-	outname="hg38_filt_"$binS"bp.bed"
-else
-	genomeSizeFile='/broad/thechenlab/atac_pipeline_bc/genomes/chrom.sizes/hg38.chrom.sizes'
-	outname="hg38_"$binS"bp.bed"
+	rm $output_name
 fi
+
+#Removes ChrY, chrM, and unplaced contigs
+filt_chrom_size=$chrom_size".filtered"
+cat $chrom_size | grep chr| grep -v chrM | grep -v Y | awk '{if(length($1)<6)print}'| sort -V -k1,1 > $filt_chrom_size
+
+#Start the bin generation
+echo "Generating "$bin_size" bp bins for "$genome_id
 
 while read line; do
 	chrName=`echo $line| awk '{print $1}'`
@@ -35,18 +43,18 @@ while read line; do
 
 	while [ $upper -lt $chrLen ]; do
 
-		((test=lower+binS+binS))
+		((test=lower+bin_size))
 
 		if [ $test -le $chrLen ]
 		then
-		((upper=lower+binS))
+			((upper=lower+bin_size))
 		else
 			upper=$chrLen
 		fi
 
-		echo -e "$chrName\t$lower\t$upper">>$outname
+		echo -e "$chrName\t$lower\t$upper">>$output_name
 		((lower=$upper))
 
 	done
 
-done <$genomeSizeFile
+done <$filt_chrom_size
